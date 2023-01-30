263a3f853e7a52a2adb794071b78746318dc0851
Amaris Laelle A. Tomas <90588185+Arielle060707@users.noreply.github.com>
2023-01-31 00:24:48 +0800

    Add files via upload

---
+++ FriendshipCrossover.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package friendshipcrossover;
+
+/**
+ *
+ * @author acer
+ */
+import java.util.Scanner;
+public class FriendshipCrossover {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Greetings, dear user! Welcome to Friendship Crossover. Would you like to quit or play?");
+        String userFirstChoice = new .nextLine();
+        
+    
+    }
+    
+}
---
+++ Player.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package friendshipcrossover;
+
+/**
+ *
+ * @author acer
+ */
+public class Player {
+    int niceWan, sadgeMomint, userPoints;
+    String givenName;
+}
